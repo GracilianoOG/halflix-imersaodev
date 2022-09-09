e8d5a361fb5ce26932a8fe2e5d117edc90cb020c
Gabriel <gabriel.gracilianomb@gmail.com>
2022-09-09 19:25:13 -0300

    Pequena melhoria na estrutura do código

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,25 @@
-const jogos = document.querySelector(".jogos__capas");
+const jogosCapas = document.querySelector(".jogos__capas");
 const campoEntradaCapa = document.querySelector(".jogos__input");
 const botaoAdicionaCapa = document.querySelector(".jogos__botao--adicionar");
 const botaoDeletaCapa = document.querySelector(".jogos__botao--deletar");
-const jogosCapas = [];
+const listaDasCapas = [];
 
 botaoAdicionaCapa.addEventListener("click", () => {
     const capa = devolveEntradaFormatada();
-    if(jogosCapas.includes(capa)) {
+    const ultimoElemento = listaDasCapas.length;
+    if(listaDasCapas.includes(capa)) {
         return;
     }
-    jogosCapas.push(capa);
-    const ultimoElemento = jogosCapas.length - 1;
-    jogos.innerHTML += `<img class="jogos__capa" src="${jogosCapas[ultimoElemento]}">`;
+    listaDasCapas.push(capa);
+    jogosCapas.innerHTML += `<img class="jogos__capa" src="${listaDasCapas[ultimoElemento]}">`;
 });
 
 botaoDeletaCapa.addEventListener("click", () => {
     const capa = devolveEntradaFormatada();
-    if(jogosCapas.includes(capa)) {
-        const posicao = jogosCapas.indexOf(capa);
-        jogosCapas.splice(posicao, 1);
-        jogos.children[posicao].outerHTML = "";
+    if(listaDasCapas.includes(capa)) {
+        const posicao = listaDasCapas.indexOf(capa);
+        listaDasCapas.splice(posicao, 1);
+        jogosCapas.children[posicao].outerHTML = "";
     }
 });
 
