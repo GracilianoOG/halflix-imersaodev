a7629a1690b203fafb0862fdd1efe7b441d17dac
Gabriel <gabriel.gracilianomb@gmail.com>
2022-09-09 19:06:47 -0300

    Botão deletar implementado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,22 @@
 const jogos = document.querySelector(".jogos__capas");
 const campoEntradaCapa = document.querySelector(".jogos__input");
 const botaoAdicionaCapa = document.querySelector(".jogos__botao--adicionar");
+const botaoDeletaCapa = document.querySelector(".jogos__botao--deletar");
 const jogosCapas = [];
 
 botaoAdicionaCapa.addEventListener("click", () => {
     if(jogosCapas.includes(campoEntradaCapa.value)) {
-        console.log("Elemento repetido!");
         return;
     }
     jogosCapas.push(campoEntradaCapa.value);
     const ultimoElemento = jogosCapas.length - 1;
     jogos.innerHTML += `<img class="jogos__capa" src="${jogosCapas[ultimoElemento]}">`;
+});
+
+botaoDeletaCapa.addEventListener("click", () => {
+    if(jogosCapas.includes(campoEntradaCapa.value)) {
+        const posicao = jogosCapas.indexOf(campoEntradaCapa.value);
+        jogosCapas.splice(posicao, 1);
+        jogos.children[posicao].outerHTML = "";
+    }
 });
